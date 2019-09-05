2c35e4d0e7ba344d76f80474a7b8f761230468fa
ran6195 <ran6195@gmail.com>
2019-09-05 12:35:35 +0200

    Terzo commit

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -5,7 +5,9 @@ class Sidebar extends React.Component {
 
     render() {
         return (
-            
+            <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            </ul>   
         )
     }
 
