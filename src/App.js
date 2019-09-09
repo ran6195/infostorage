5e10e29bfdcaa118f1a0357991090329712a4d09
ran6195 <ran6195@gmail.com>
2019-09-09 09:39:24 +0200

    Modificata variabile per sideba

--- src/App.js
+++ src/App.js
@@ -19,7 +19,14 @@ const sidebar = {
     icona : ''
   } ,
   voci : [
-    { tipo: 'singola' , titolo: 'Prima voce'}
+    { tipo: 'singola' , titolo: 'Prima voce'} ,
+    { tipo: 'divider' } ,
+    { tipo: 'heading' , titolo: 'interface' } ,
+    { tipo: 'menu' , titolo: 'components' , voci: [
+      { tipo: 'heading' , titolo: 'custom components' } ,
+      { tipo: 'singola' , titolo: 'Buttons'} ,
+      { tipo: 'singola' , titolo: 'Cards'}
+    ] } 
   ]
 }
 
