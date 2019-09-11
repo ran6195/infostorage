906d9e85b003bc11c91a90be0a8fdd1fe3712c04
ran6195 <ran6195@gmail.com>
2019-09-11 23:30:21 +0200

    gestione dello stato dei menu (non comp.)

--- src/components/sidebar/ItemCollapsible.js
+++ src/components/sidebar/ItemCollapsible.js
@@ -1,90 +1,89 @@
-import React from 'react'
-import SubItem from './SubItem'
-import _ from 'lodash'
-import SubMenuHeader from './SubMenuHeader'
+import React from "react";
+import SubItem from "./SubItem";
+import _ from "lodash";
+import SubMenuHeader from "./SubMenuHeader";
 
 class ItemCollapsible extends React.Component {
-    constructor( props ) {
-        super( props )
+  constructor(props) {
+    super(props);
 
-        this.handleClick = this.handleClick.bind( this )
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    //console.log( e.target )
+    const tag = e.target.tagName;
+    let el = e.target;
+    let id = "";
+
+    switch (tag) {
+      case "A":
+        id = el.dataset.target;
+        break;
+
+      case "I":
+      case "SPAN":
+        id = el.parentElement.dataset.target;
+        el = el.parentElement;
+        break;
+
+      default:
     }
 
+    document.getElementById(id).classList.contains("show")
+      ? document.getElementById(id).classList.remove("show")
+      : document.getElementById(id).classList.add("show");
 
+    el.classList.contains("collapsed")
+      ? el.classList.remove("collapsed")
+      : el.classList.add("collapsed");
+  }
 
+  render() {
+    /**
+     * costruzione delle voci del sotto menu
+     */
 
-    handleClick( e ) {
-        e.preventDefault()
-        //console.log( e.target )
-        const tag = e.target.tagName
-        let el  = e.target
-        let id = ''
-        //testing branches
-        const prova = {}
+    let subMenu = [];
+    let k = 0;
+    _.each(this.props.voci, voce => {
+      switch (voce.tipo) {
+        case "singola":
+          subMenu.push(<SubItem href="#" titolo={voce.titolo} key={k++} />);
+          break;
 
+        case "header":
+          subMenu.push(<SubMenuHeader titolo={voce.titolo} key={k++} />);
+          break;
 
-        switch( tag ) {
-            case 'A':
-                id = el.dataset.target
-            break;
+        default:
+      }
+    });
 
-            case 'I':
-            case 'SPAN':
-                id = el.parentElement.dataset.target
-                el = el.parentElement
-            break;
-
-            default:
-
-        }
-
-        document.getElementById( id ).classList.contains( 'show' ) ? document.getElementById( id ).classList.remove( 'show' ) : document.getElementById( id ).classList.add( 'show' )
-
-        el.classList.contains( 'collapsed' ) ? el.classList.remove( 'collapsed' ) : el.classList.add( 'collapsed' )
-    }
-
-    render() {
-        /**
-         * costruzione delle voci del sotto menu
-         */
-
-        let subMenu = []
-        let k = 0
-        _.each( this.props.voci , voce => {
-            switch( voce.tipo ) {
-                case "singola":
-                    subMenu.push (
-                        <SubItem href="#" titolo={ voce.titolo } key={ k++ } />
-                    )
-                break;
-                
-                case "header":
-                    subMenu.push (
-                        <SubMenuHeader titolo={ voce.titolo } key={ k++ } />
-                    )
-                break;
-
-                default:
-            }
-
-        })
-
-
-        return (
-            <li className="nav-item">
-                <a onClick={ this.handleClick } className="nav-link collapsed" href="index.html" data-toggle="collapse" data-target={ this.props.idCollapse } aria-expanded="true" aria-controls="collapseTwo">
-                    <i className={ this.props.icona }></i>
-                    <span>{ this.props.titolo }</span>
-                </a>
-                <div id={ this.props.idCollapse } className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div className="bg-white py-2 collapse-inner rounded">
-                        { subMenu }
-                    </div>
-                </div>
-            </li>
-        )
-    }
-
+    return (
+      <li className="nav-item">
+        <a
+          onClick={this.handleClick}
+          className="nav-link collapsed"
+          href="index.html"
+          data-toggle="collapse"
+          data-target={this.props.idCollapse}
+          aria-expanded="true"
+          aria-controls="collapseTwo">
+          <i className={this.props.icona}></i>
+          <span>{this.props.titolo}</span>
+        </a>
+        <div
+          id={this.props.idCollapse}
+          className="collapse"
+          aria-labelledby="headingTwo"
+          data-parent="#accordionSidebar">
+          <div className="bg-white py-2 collapse-inner rounded">{subMenu}</div>
+        </div>
+      </li>
+    );
+  }
 }
 
-export default ItemCollapsible
+export default ItemCollapsible;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 //import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 const sidebar = {
-    brand : {
-      titolo : 'infostorage' ,
-      icona : 'fas fa-database'
-    } ,
-    voci : [
-      { tipo: 'singola' , titolo: 'Prima voce' , icona: "fas fa-fw fa-tachometer-alt" } ,
-      { tipo: 'divider' , margin: false } ,
-      { tipo: 'heading' , titolo: 'Heading' } ,
-      
-      { tipo: 'singola' , titolo: 'seconda voce' , icona: "fas fa-fw fa-tachometer-alt" } ,
-      { tipo: 'singola' , titolo: 'terza voce' , icona: "fas fa-fw fa-tachometer-alt" } ,
-      { tipo: 'singola' , titolo: 'Prima voce' , icona: "fas fa-fw fa-tachometer-alt" } ,
-      { 
-        tipo: 'menu' , 
-        titolo: 'Components' ,  
-        titoloSubMenu: "Custom components" , 
-        icona : "fas fa-fw fa-cog" ,
-        voci: [
-          { tipo : 'header' , titolo : "login screens" } ,
-          { tipo: 'singola' , titolo: 'Buttons'} ,
-          { tipo: 'singola' , titolo: 'Cards'} ,
-          { tipo : 'header' , titolo : "Other pages" } ,
-          { tipo: 'singola' , titolo: 'Buttons 2'} ,
-          { tipo: 'singola' , titolo: 'Cards 2'}
-      ] } 
-    ]
-  }
+  brand: {
+    titolo: "infostorage",
+    icona: "fas fa-database"
+  },
+  voci: [
+    {
+      tipo: "singola",
+      titolo: "Prima voce",
+      icona: "fas fa-fw fa-tachometer-alt"
+    },
+    { tipo: "divider", margin: false },
+    { tipo: "heading", titolo: "Heading" },
 
+    {
+      tipo: "singola",
+      titolo: "seconda voce",
+      icona: "fas fa-fw fa-tachometer-alt"
+    },
+    {
+      tipo: "singola",
+      titolo: "terza voce",
+      icona: "fas fa-fw fa-tachometer-alt"
+    },
+    {
+      tipo: "singola",
+      titolo: "Prima voce",
+      icona: "fas fa-fw fa-tachometer-alt"
+    },
+    {
+      tipo: "menu",
+      titolo: "Components",
+      titoloSubMenu: "Custom components",
+      icona: "fas fa-fw fa-cog",
+      chiuso: true,
+      voci: [
+        { tipo: "header", titolo: "login screens" },
+        { tipo: "singola", titolo: "Buttons" },
+        { tipo: "singola", titolo: "Cards" },
+        { tipo: "header", titolo: "Other pages" },
+        { tipo: "singola", titolo: "Buttons 2" },
+        { tipo: "singola", titolo: "Cards 2" }
+      ]
+    },
+    {
+      tipo: "menu",
+      titolo: "Menu di prova",
+      titoloSubMenu: "Sotto menu di prva",
+      icona: "fas fa-fw fa-database",
+      chiuso: true,
+      voci: [
+        { tipo: "header", titolo: "login screens" },
+        { tipo: "singola", titolo: "Buttons" },
+        { tipo: "singola", titolo: "Cards" },
+        { tipo: "header", titolo: "Other pages" },
+        { tipo: "singola", titolo: "Buttons 2" },
+        { tipo: "singola", titolo: "Cards 2" }
+      ]
+    }
+  ]
+};
 
-
-ReactDOM.render(<App sidebar={ sidebar } />, document.getElementById('wrapper'));
+ReactDOM.render(<App sidebar={sidebar} />, document.getElementById("wrapper"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
