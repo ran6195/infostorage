ae582aaed3d2221d990a3af6bc7e5b4763311e52
toniogiamp <toniog@gmail.com>
2019-09-16 10:24:31 +0200

    test new brach

--- src/index.js
+++ src/index.js
@@ -14,59 +14,85 @@ const sidebar = {
       tipo: "singola",
       titolo: "Prima voce",
       icona: "fas fa-fw fa-tachometer-alt"
-    },
-    { tipo: "divider", margin: false },
-    { tipo: "heading", titolo: "Heading" },
-
-    {
+    }, {
+      tipo: "divider",
+      margin: false
+    }, {
+      tipo: "heading",
+      titolo: "Heading"
+    }, {
       tipo: "singola",
       titolo: "seconda voce",
       icona: "fas fa-fw fa-tachometer-alt"
-    },
-    {
+    }, {
       tipo: "singola",
       titolo: "terza voce",
       icona: "fas fa-fw fa-tachometer-alt"
-    },
-    {
+    }, {
       tipo: "singola",
       titolo: "Prima voce",
       icona: "fas fa-fw fa-tachometer-alt"
-    },
-    {
+    }, {
       tipo: "menu",
       titolo: "Components",
       titoloSubMenu: "Custom components",
       icona: "fas fa-fw fa-cog",
       chiuso: true,
       voci: [
-        { tipo: "header", titolo: "login screens" },
-        { tipo: "singola", titolo: "Buttons" },
-        { tipo: "singola", titolo: "Cards" },
-        { tipo: "header", titolo: "Other pages" },
-        { tipo: "singola", titolo: "Buttons 2" },
-        { tipo: "singola", titolo: "Cards 2" }
+        {
+          tipo: "header",
+          titolo: "login screens"
+        }, {
+          tipo: "singola",
+          titolo: "Buttons"
+        }, {
+          tipo: "singola",
+          titolo: "Cards"
+        }, {
+          tipo: "header",
+          titolo: "Other pages"
+        }, {
+          tipo: "singola",
+          titolo: "Buttons 2"
+        }, {
+          tipo: "singola",
+          titolo: "Cards 2"
+        }
       ]
-    },
-    {
+    }, {
       tipo: "menu",
       titolo: "Menu di prova",
-      titoloSubMenu: "Sotto menu di prva",
+      titoloSubMenu: "Sotto menu di prova",
       icona: "fas fa-fw fa-database",
       chiuso: true,
       voci: [
-        { tipo: "header", titolo: "login screens" },
-        { tipo: "singola", titolo: "Buttons" },
-        { tipo: "singola", titolo: "Cards" },
-        { tipo: "header", titolo: "Other pages" },
-        { tipo: "singola", titolo: "Buttons 2" },
-        { tipo: "singola", titolo: "Cards 2" }
+        {
+          tipo: "header",
+          titolo: "login screens"
+        }, {
+          tipo: "singola",
+          titolo: "Buttons"
+        }, {
+          tipo: "singola",
+          titolo: "Cards"
+        }, {
+          tipo: "header",
+          titolo: "Other pages"
+        }, {
+          tipo: "singola",
+          titolo: "Buttons 2"
+        }, {
+          tipo: "singola",
+          titolo: "Cards 2"
+        }
       ]
     }
   ]
 };
 
-ReactDOM.render(<App sidebar={sidebar} />, document.getElementById("page-top"));
+ReactDOM.render(< App sidebar = {
+  sidebar
+} />, document.getElementById("page-top"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
