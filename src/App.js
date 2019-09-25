f44699ef79f9b5ce6e5f52605b465401df8269f1
Francesco Corsanello <francesco@MBPdiFrancesco.homenet.telecomitalia.it>
2019-09-26 00:39:38 +0200

    pagina di login funzionante

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './fontawesome.css'
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 //import 'bootstrap/dist/css/bootstrap.min.css'
-import Axios from 'axios'
+
 
 import Sidebar from './components/sidebar/Sidebar'
 import ContentWrapper from './components/content/ContentWrapper';
@@ -21,52 +21,9 @@ const MyContext = React.createContext({
 
 class App extends React.Component {
 
-  constructor( props ) {
-    super( props )
-    this.handleLogin = this.handleLogin.bind( this )
-  }
-
-  handleLogin( e ) {
-    e.preventDefault();
-    this.setState({ logged_in : true })
-  }
-
-  componentDidMount() {
-    Axios.defaults.headers.post['Content-Type'] = 'text/json';
-   
-    Axios.post( 'https:///api.opsboard.cloud/login' , {
-      user: 'toniog@gmail.com' ,
-      pass : 'pippo'
-    })
-    .then( res => console.log( res.data ) )
-    .catch( err => console.log( err ) )
-  }
-
-/*   componentDidMount() {
-
-    let myHeaders = new Headers({
-      "Content-Type" : "text/html; charset=UTF-8"
-      }     
-    );
-
-    let myInit = { method: 'POST',
-               headers: myHeaders,
-               mode: 'cors',
-               cache: 'default' };
-    fetch( 'https://api.opsboard.cloud/login' , myInit , {
-      user: 'toniog@gmail.com' ,
-      pass: 'pippo'
-    })
-
-   } */
-
   render() {
 
-    let token = MyContext.Provider.token;
-
-
-    console.log( token )
-    if( false ) {
+    if( this.props.logged_in ) {
       return (
         <MyContext.Provider value={ this.state }>
         <div id="wrapper">
@@ -78,16 +35,14 @@ class App extends React.Component {
         </MyContext.Provider>
       )
     } else {
-      document.getElementsByTagName( 'body' )[ 0 ].classList.add( 'bg-gradient-primary' )
+      
       return (
-          <Login />
+          <Login logged_in={ this.props.logged_in } sidebar={ this.props.sidebar } />
       )
     }
     
   }
 }
-
-
 
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,68 +1,90 @@
 import React from 'react';
+//import { BrowserRouter as Router , Redirect } from 'react-router-dom'
+import App from '../App'
+import Axios from 'axios'
 
 class Login extends React.Component {
 
     constructor( props ) {
         super( props )
-        //this.state = { logged_in : props.logged_in }
+        //console.log( this.props )
+        this.state = { logged_in : this.props.logged_in }
         this.handleLogin = this.handleLogin.bind( this )
     }
 
     handleLogin( e ) {
         e.preventDefault();
-        this.setState({ logged_in : true })
+        Axios.post( 'https://api.opsboard.cloud/login' , {
+            user: document.getElementById( 'user' ).value,
+            pass: document.getElementById( 'pass' ).value
+        }).then( res => {
+            if( res.data.token !== undefined ) {
+                this.setState({ logged_in : true })
+            } else {
+                console.log( res.data.errore )
+            }
+        })
+        //this.setState({ logged_in : true })
     }
 
     render() {
+
+        if( this.state.logged_in ) {
+            return (
+                <App logged_in={ this.state.logged_in } sidebar={ this.props.sidebar }/>
+            )
+        } else {
+
         return (
-            <div className="container">
-                <div className="card o-hidden border-0 shadow-lg" style={{ marginTop: 200 }}>
-                    <div className="card-body p-0">
-                    <div className="row">
-                        <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                        <div className="col-lg-6">
-                        <div className="p-5">
-                            <div className="text-center">
-                            <h1 className="h4 text-gray-900 mb-4">Effettua il login</h1>
-                            <form className="user">
-                                <div className="form-group">
-                                <input type="email" className="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." />
+                <div className="container">
+                    <div className="card o-hidden border-0 shadow-lg" style={{ marginTop: 200 }}>
+                        <div className="card-body p-0">
+                        <div className="row">
+                            <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div className="col-lg-6">
+                            <div className="p-5">
+                                <div className="text-center">
+                                <h1 className="h4 text-gray-900 mb-4">Effettua il login</h1>
+                                <form className="user">
+                                    <div className="form-group">
+                                    <input type="email" className="form-control form-control-user" id="user" aria-describedby="emailHelp" placeholder="Enter Email Address..." />
+                                    </div>
+                                    <div className="form-group">
+                                    <input type="password" className="form-control form-control-user" id="pass" placeholder="Password" />
+                                    </div>
+                                    {/* <div className="form-group">
+                                    <div className="custom-control custom-checkbox small">
+                                        <input type="checkbox" className="custom-control-input" id="customCheck" />
+                                        <label className="custom-control-label" htmlFor="customCheck">Remember Me</label>
+                                    </div>
+                                    </div> */}
+                                    <button onClick={ this.handleLogin } className="btn btn-primary btn-user btn-block">
+                                    Login
+                                    </button>
+                                {/*  <hr />
+                                    <a href="index.html" className="btn btn-google btn-user btn-block">
+                                    <i className="fab fa-google fa-fw"></i> Login with Google
+                                    </a>
+                                    <a href="index.html" className="btn btn-facebook btn-user btn-block">
+                                    <i className="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                    </a> */}
+                                </form>
+                            <hr></hr>
+                        {/*       <div class="text-center">
+                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="register.html">Create an Account!</a>
+                            </div> */}
                                 </div>
-                                <div className="form-group">
-                                <input type="password" className="form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
-                                </div>
-                                {/* <div className="form-group">
-                                <div className="custom-control custom-checkbox small">
-                                    <input type="checkbox" className="custom-control-input" id="customCheck" />
-                                    <label className="custom-control-label" htmlFor="customCheck">Remember Me</label>
-                                </div>
-                                </div> */}
-                                <button onClick={ this.handleLogin } className="btn btn-primary btn-user btn-block">
-                                Login
-                                </button>
-                            {/*  <hr />
-                                <a href="index.html" className="btn btn-google btn-user btn-block">
-                                <i className="fab fa-google fa-fw"></i> Login with Google
-                                </a>
-                                <a href="index.html" className="btn btn-facebook btn-user btn-block">
-                                <i className="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                </a> */}
-                            </form>
-                        <hr></hr>
-                    {/*       <div class="text-center">
-                            <a class="small" href="forgot-password.html">Forgot Password?</a>
-                        </div>
-                        <div class="text-center">
-                            <a class="small" href="register.html">Create an Account!</a>
-                        </div> */}
+                            </div>
                             </div>
                         </div>
                         </div>
                     </div>
-                    </div>
-                </div>
-            </div> 
-        )
+                </div> 
+            )
+        }
     }
 
 
