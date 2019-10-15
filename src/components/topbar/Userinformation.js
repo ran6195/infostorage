1e47dc33e58edfd059aac6d2747ac9eddd1aefb8
ran6195 <ran6195@gmail.com>
2019-10-15 13:20:12 +0200

    Prova dropdown menu custom

--- src/components/topbar/Userinformation.js
+++ src/components/topbar/Userinformation.js
@@ -18,10 +18,12 @@ class CustomToggle extends React.Component {
 
   render() {
     return (
-      <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-	      <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
-	      <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60" />
-      </a>
+      <li className="nav-item dropdown no-arrow">
+        <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <span className="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
+          <img className="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60" />
+        </a>
+      </li>
     );
   }
 }
